a1d08f8d8eee6b5299765dc10009f1c09417a179
Brian Bachow <bmbachow@gmail.com>
2022-02-27 11:41:48 -0800

    set up firebase

--- Messenger/Controllers/Login/LoginViewController.swift
+++ Messenger/Controllers/Login/LoginViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class LoginViewController: UIViewController {
 
@@ -160,6 +161,16 @@ class LoginViewController: UIViewController {
         }
         
         // firebase login
+        FirebaseAuth.Auth.auth().signIn(withEmail: email, password: password, completion: { authResult, error in
+            guard let result = authResult, error == nil else {
+                print("failed to log in user with email: \(email)")
+                return
+            }
+            
+            let user = result.user
+            print("Logged In User: \(user)")
+            
+        })
         
     }
     
